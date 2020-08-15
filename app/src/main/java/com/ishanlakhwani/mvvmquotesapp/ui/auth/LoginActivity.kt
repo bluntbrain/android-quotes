a8baf288964fbe231a3b6f8f7fbf44e0a5486bed
bluntbrain <ishanlakhwani@hotmail.com>
2020-08-15 08:56:40 +0530

    added dependencies and layout

--- app/src/main/java/com/ishanlakhwani/mvvmquotesapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/ishanlakhwani/mvvmquotesapp/ui/auth/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.ishanlakhwani.mvvmquotesapp
+package com.ishanlakhwani.mvvmquotesapp.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ishanlakhwani.mvvmquotesapp.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
